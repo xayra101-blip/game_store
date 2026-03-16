f0e21333d7c1bb18a2c5a22c4f1e4beb9c709432
xayra101-blip <xayra101@gmail.com>
2026-03-16 11:48:53 +0700

    Force HTTPS scheme and refactor view composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,32 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\ProductType;
 use App\Cart;
 use Session;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        URL::forceScheme('https');
+
         view()->composer('header',function($view){
             $loai_sp = ProductType::all();
-            
             $view->with('loai_sp',$loai_sp);
         });
 
@@ -36,7 +28,12 @@ class AppServiceProvider extends ServiceProvider
             if(Session('cart')){
                 $oldCart = Session::get('cart');
                 $cart = new Cart($oldCart);
-                $view->with(['cart'=>Session::get('cart'), 'product_cart'=>$cart->items,'totalPrice'=>$cart->totalPrice,'totalQty'=>$cart->totalQty]);
+                $view->with([
+                    'cart'=>Session::get('cart'),
+                    'product_cart'=>$cart->items,
+                    'totalPrice'=>$cart->totalPrice,
+                    'totalQty'=>$cart->totalQty
+                ]);
             }
         });
     }
